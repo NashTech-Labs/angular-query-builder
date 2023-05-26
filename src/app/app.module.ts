81485450d7e6e52d24a55a5b006c1b0ba6ed728d
Anirudh <anirudh.chauhan@knoldus.com>
2023-05-26 12:53:28 +0530

    query-builder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { QueryBuilderModule } from 'angular2-query-builder';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QueryBuilderModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCardModule,
+    FormsModule,
+    MatRadioModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCardModule,
+    MatRadioModule,
+    MatIconModule,
+    BrowserModule,
+    MatButtonModule
+  ],
 })
-export class AppModule { }
+export class AppModule {}
